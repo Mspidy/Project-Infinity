a7a54fd406c7321ab52d297bb50320e49f07fe67
Prabhat Dubey <prabhat.cioreview@gmail.com>
2023-01-07 15:54:31 +0530

    Modifications with json data

--- src/app/footar/footar.component.ts
+++ src/app/footar/footar.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-footar',
   templateUrl: './footar.component.html',
   styleUrls: ['./footar.component.css']
 })
 export class FootarComponent {
-
+  lat: number = 52.488328;
+  lng: number = 8.717017;
 }
---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,7 @@
+export const environment = {
+    production: true,
+    //apiURL: "https://api.quantiinvest.com/core_v2/api/",
+    // apiURL: "/core_v2/api/",
+    // authURL: "https://quanti-invest-py.herokuapp.com/"
+  };
+                                                                                                                                                                                                                                                                                    
