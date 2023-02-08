4e1b8a6da3749f043bdda6939761c29a74dc65e9
Prabhat Dubey <prabhat.cioreview@gmail.com>
2023-02-09 01:55:30 +0530

    modifications with new Finance stats components

--- src/app/datamanagement/datamanagement.component.ts
+++ src/app/datamanagement/datamanagement.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { LotsdataService } from '../lotsdata.service';
+import * as Highcharts from "highcharts";
 
 
 
@@ -15,9 +16,124 @@ export class DatamanagementComponent {
   }
 
   sectorData:any =[]
+  private getRandomNumber(min: number, max: number): number {
+    return Math.floor(Math.random() * (max - min + 1) + min)
+  }
 
   ngOnInit(){
-    
+    this.createChartLine();
+    this.createChartColumn()
+  }
+  private createChartLine(): void {
+    let date = new Date();
+    const data: any[] = [];
+
+    for (let i = 0; i < 10; i++) {
+      date.setDate(new Date().getDate() + i);
+      data.push([`${date.getDate()}/${date.getMonth() + 1}`, this.getRandomNumber(0, 1000)]);
+    }
+
+    const chart = Highcharts.chart('chart-line', {
+      chart: {
+        type: 'line',
+      },
+      title: {
+        text: 'Line Chart',
+      },
+      credits: {
+        enabled: false,
+      },
+      legend: {
+        enabled: false,
+      },
+      yAxis: {
+        title: {
+          text: null,
+          
+        }
+      },
+      xAxis: {
+        type: 'category',
+      },
+      tooltip: {
+        headerFormat: `<div>Date: {point.key}</div>`,
+        pointFormat: `<div>{series.name}: {point.y}</div>`,
+        shared: true,
+        useHTML: true,
+      },
+      series: [{
+        name: 'Amount',
+        data,
+        color: '#0d174c'
+      }],
+    } as any);
+
+    setInterval(() => {
+      date.setDate(date.getDate() + 1);
+      chart.series[0].addPoint([`${date.getDate()}/${date.getMonth() + 1}`, this.getRandomNumber(0, 1000)], true, true);
+    }, 1500);
+  }
+
+  private createChartColumn(): void {
+    let date = new Date();
+    const data: any[] = [];
+
+    for (let i = 0; i < 10; i++) {
+      date.setDate(new Date().getDate() + i);
+      data.push({
+        name: `${date.getDate()}/${date.getMonth() + 1}`,
+        y: this.getRandomNumber(0, 1000),
+      });
+    }
+
+    const chart = Highcharts.chart('chart-column' as any, {
+      
+      chart: {
+        type: 'areaspline',
+      },
+      title: {
+        text: 'Column Chart',
+      },
+      credits: {
+        enabled: false,
+      },
+      legend: {
+        enabled: false,
+      },
+      yAxis: {
+        min: 0,
+        title: undefined,
+      },
+      xAxis: {
+        type: 'category',
+      },
+      tooltip: {
+        headerFormat: `<div>Date: {point.key}</div>`,
+        pointFormat: `<div>{series.name}: {point.y}</div>`,
+        shared: true,
+        useHTML: true,
+      },
+      plotOptions: {
+        bar: {
+          dataLabels: {
+            enabled: true,
+          },
+        },
+      },
+      series: [{
+        name: 'Amount',
+        data,
+        color: '#0d174c'
+      }],
+    } as any);
+
+    setInterval(() => {
+      date.setDate(date.getDate() + 1);
+      chart.series[0].addPoint({
+        name: `${date.getDate()}/${date.getMonth() + 1}`,
+        y: this.getRandomNumber(0, 1000),
+      }, true, true);
+    }, 1500);
   }
 
 }
