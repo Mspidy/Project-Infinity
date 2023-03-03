ae40e345a39b37743338a55427b1994d289eb7e8
Prabhat Dubey <prabhat.cioreview@gmail.com>
2023-03-03 16:10:37 +0530

    New change with add-blogs sections & also modified new API keys

--- src/app/lotsdata.service.ts
+++ src/app/lotsdata.service.ts
@@ -11,32 +11,32 @@ export class LotsdataService {
   constructor(public http:HttpClient) {}
 
   getAllNews(): Observable<any>{
-    let url = `https://newsapi.org/v2/top-headlines?country=in&category=technology&apiKey=ecdf9d0d58464f68b9383211c9bdcf1f`
+    let url = `https://newsapi.org/v2/top-headlines?country=in&category=technology&apiKey=816419abdc3f49a6a9ea8cba3356cc66`
     return this.http.get(url)
   }
 
   getAllSportsNews(): Observable<any>{
-    let url = `https://newsapi.org/v2/top-headlines?country=in&category=sports&apiKey=ecdf9d0d58464f68b9383211c9bdcf1f`
+    let url = `https://newsapi.org/v2/top-headlines?country=in&category=sports&apiKey=816419abdc3f49a6a9ea8cba3356cc66`
     return this.http.get(url)
   }
 
   getAllEntertainmentsNews(): Observable<any>{
-    let url = `https://newsapi.org/v2/top-headlines?country=in&category=entertainment&apiKey=ecdf9d0d58464f68b9383211c9bdcf1f`
+    let url = `https://newsapi.org/v2/top-headlines?country=in&category=entertainment&apiKey=816419abdc3f49a6a9ea8cba3356cc66`
     return this.http.get(url)
   }
 
   getAllHealthsNews(): Observable<any>{
-    let url = `https://newsapi.org/v2/top-headlines?country=in&category=health&apiKey=ecdf9d0d58464f68b9383211c9bdcf1f`
+    let url = `https://newsapi.org/v2/top-headlines?country=in&category=health&apiKey=816419abdc3f49a6a9ea8cba3356cc66`
     return this.http.get(url)
   }
 
   getAllScienceNews(): Observable<any>{
-    let url = `https://newsapi.org/v2/top-headlines?country=in&category=science&apiKey=ecdf9d0d58464f68b9383211c9bdcf1f`
+    let url = `https://newsapi.org/v2/top-headlines?country=in&category=science&apiKey=816419abdc3f49a6a9ea8cba3356cc66`
     return this.http.get(url)
   }
 
   getAllBusinessNews(): Observable<any>{
-    let url = `https://newsapi.org/v2/top-headlines?country=in&category=business&apiKey=ecdf9d0d58464f68b9383211c9bdcf1f`
+    let url = `https://newsapi.org/v2/top-headlines?country=in&category=business&apiKey=816419abdc3f49a6a9ea8cba3356cc66`
     return this.http.get(url)
   }
 
